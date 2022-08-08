52067fe3c8d1212d64403ee6bcd9629ac106e21e
yousef404X <sarisssko@gmail.com>
2022-08-08 16:46:39 +0300

    done like always

--- hw1/app/src/main/java/com/example/homework/MainActivity.java
+++ hw1/app/src/main/java/com/example/homework/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int equal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,34 +19,47 @@ public class MainActivity extends AppCompatActivity {
         EditText Ed2 = findViewById(R.id.ET2);
         EditText Ed3 = findViewById(R.id.ET3);
         EditText Ed4 = findViewById(R.id.ET4);
-        Button b = findViewById(R.id.calculate);
+        Button add = findViewById(R.id.add);
         TextView t = findViewById(R.id.result);
-        Button Reset = findViewById(R.id.Reset);
-        Reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Ed1.setText("");
-                Ed2.setText("");
-                Ed3.setText("");
-                Ed4.setText("");
-                t.setText("result");
-            }
-        });
-        b.setOnClickListener(new View.OnClickListener() {
+        Button minus = findViewById(R.id.minus);
+        Button calculate = findViewById(R.id.calculate);
+
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int num1 = Integer.parseInt(Ed1.getText().toString());
                 int num2 = Integer.parseInt(Ed2.getText().toString());
                 int num3 = Integer.parseInt(Ed3.getText().toString());
                 int num4 = Integer.parseInt(Ed4.getText().toString());
-                t.setText(totalGrade(num1,num2,num3,num4) + " " );
+                equal = totalGrade(num1,num2,num3,num4);
+
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1 = Integer.parseInt(Ed1.getText().toString());
+                int num2 = Integer.parseInt(Ed2.getText().toString());
+                int num3 = Integer.parseInt(Ed3.getText().toString());
+                int num4 = Integer.parseInt(Ed4.getText().toString());
+                equal = num1 - num2 - num3 - num4;
+            }
+        });
+
+
+
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t.setText(equal + " ");
             }
         });
     }
 
-    public static int totalGrade(int num1, int num2, int num3, int num4){
-
-
+    public static int totalGrade(int num1,int num2,int num3,int num4){
         return num1 + num2 + num3 + num4;
+
     }
 }
